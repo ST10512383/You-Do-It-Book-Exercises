8e5e81a542452c41b9fca80ff5cc944aebef0f2b
Cailen Boshoff <ST10512383@myemeris.edu.za>
2026-03-10 13:47:31 +0200

    Create ArithmeticDemo.java

---
+++ ArithmeticDemo/src/main/java/com/mycompany/arithmeticdemo/ArithmeticDemo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.arithmeticdemo;
+
+/**
+ *
+ * @author CALBIX
+ */
+public class ArithmeticDemo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
