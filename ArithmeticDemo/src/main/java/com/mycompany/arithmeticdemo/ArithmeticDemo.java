ec4adeb9d1c4dc6c80c02c03b3daa11c0da00e16
Cailen Boshoff <ST10512383@myemeris.edu.za>
2026-03-10 14:04:14 +0200

    Add arithmetic variables

--- ArithmeticDemo/src/main/java/com/mycompany/arithmeticdemo/ArithmeticDemo.java
+++ ArithmeticDemo/src/main/java/com/mycompany/arithmeticdemo/ArithmeticDemo.java
@@ -19,5 +19,8 @@ public class ArithmeticDemo {
         System.out.println("Please enter the second number -> ");
         secondNumber = input.nextInt();
         
+        sum = firstNumber + secondNumber;
+        difference = firstNumber + secondNumber;
+        average = sum / 2;
     }
 }
