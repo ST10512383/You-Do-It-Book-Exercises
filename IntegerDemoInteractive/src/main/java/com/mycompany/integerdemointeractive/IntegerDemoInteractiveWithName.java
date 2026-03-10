2739c416f110794c9868b94f193a28706d699667
Cailen Boshoff <ST10512383@myemeris.edu.za>
2026-03-10 11:30:54 +0200

    Add Structure with new string input

--- IntegerDemoInteractive/src/main/java/com/mycompany/integerdemointeractive/IntegerDemoInteractiveWithName.java
+++ IntegerDemoInteractive/src/main/java/com/mycompany/integerdemointeractive/IntegerDemoInteractiveWithName.java
@@ -1,5 +1,30 @@
 package com.mycompany.integerdemointeractive;
 
+import java.util.Scanner;
+
 public class IntegerDemoInteractiveWithName {
-    
+    public static void main(String[] args){
+        Scanner userInput = new Scanner(System.in);
+        
+        String name;
+        int anInt;
+        byte aByte;
+        short aShort;
+        long aLong;
+        
+        System.out.print("Please enter an Integer > ");
+        anInt = userInput.nextInt();
+        
+        System.out.print("Please enter a Byte integer > ");
+        aByte = userInput.nextByte();
+        
+        System.out.print("Please enter a Short integer > ");
+        aLong = userInput.nextLong();
+        
+        System.out.print("Please enter your name > ");
+        name = userInput.nextLine();
+        System.out.println("Thank you, " + name);
+        
+        userInput.close();
+    }
 }
