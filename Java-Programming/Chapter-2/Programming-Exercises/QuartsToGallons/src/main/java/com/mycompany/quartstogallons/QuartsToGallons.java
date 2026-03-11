0e772683b5af5daff4630ecce5097054fe4d9c43
Cailen Boshoff <ST10512383@myemeris.edu.za>
2026-03-11 16:18:32 +0200

    Add Scanner import

--- Java-Programming/Chapter-2/Programming-Exercises/QuartsToGallons/src/main/java/com/mycompany/quartstogallons/QuartsToGallons.java
+++ Java-Programming/Chapter-2/Programming-Exercises/QuartsToGallons/src/main/java/com/mycompany/quartstogallons/QuartsToGallons.java
@@ -1,8 +1,13 @@
 package com.mycompany.quartstogallons;
 
+import java.util.Scanner;
+
 public class QuartsToGallons {
 
     public static void main(String[] args) {
+        
+        Scanner input = new Scanner(System.in);
+        
         final double QUARTS = 4;
         double quartsNeeded = 18;
         double leftOverQuarts = ((quartsNeeded / QUARTS) - 4) * 4;
