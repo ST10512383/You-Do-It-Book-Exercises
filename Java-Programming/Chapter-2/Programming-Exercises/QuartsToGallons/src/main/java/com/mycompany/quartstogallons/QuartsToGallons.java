87f8d772cd666783d0b3cd1b8e5caaa0da840dab
Cailen Boshoff <ST10512383@myemeris.edu.za>
2026-03-11 16:16:45 +0200

    Add calculation and Print statement

--- Java-Programming/Chapter-2/Programming-Exercises/QuartsToGallons/src/main/java/com/mycompany/quartstogallons/QuartsToGallons.java
+++ Java-Programming/Chapter-2/Programming-Exercises/QuartsToGallons/src/main/java/com/mycompany/quartstogallons/QuartsToGallons.java
@@ -5,6 +5,8 @@ public class QuartsToGallons {
     public static void main(String[] args) {
         final double QUARTS = 4;
         double quartsNeeded = 18;
+        double leftOverQuarts = ((quartsNeeded / QUARTS) - 4) * 4;
         
+        System.out.println("A job that needs " + (int) quartsNeeded + " quarts requires " + (int) QUARTS + " gallons plus " + (int) leftOverQuarts + " quarts");
     }
 }
