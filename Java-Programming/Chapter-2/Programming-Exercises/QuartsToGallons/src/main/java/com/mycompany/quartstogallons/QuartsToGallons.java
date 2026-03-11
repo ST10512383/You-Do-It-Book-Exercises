a71631fff96109882270f143a1552ff2619fbc47
Cailen Boshoff <ST10512383@myemeris.edu.za>
2026-03-11 16:16:21 +0200

    Add required constant and variable

--- Java-Programming/Chapter-2/Programming-Exercises/QuartsToGallons/src/main/java/com/mycompany/quartstogallons/QuartsToGallons.java
+++ Java-Programming/Chapter-2/Programming-Exercises/QuartsToGallons/src/main/java/com/mycompany/quartstogallons/QuartsToGallons.java
@@ -3,6 +3,8 @@ package com.mycompany.quartstogallons;
 public class QuartsToGallons {
 
     public static void main(String[] args) {
+        final double QUARTS = 4;
+        double quartsNeeded = 18;
         
     }
 }
