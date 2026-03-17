d5909abd29d9aa005e323c2beb3cf2fd04884f71
Cailen Boshoff <ST10512383@myemeris.edu.za>
2026-03-17 13:54:23 +0200

    Create ParadiseInfo2.java

---
+++ Java-Programming/Chapter-3/You-Do-it/ParadiseInfo/src/main/java/com/mycompany/paradiseinfo/ParadiseInfo2.java
@@ -0,0 +1,15 @@
+package com.mycompany.paradiseinfo;
+
+public class ParadiseInfo2 {
+
+    public static void main(String[] args) {
+        // Invoke displayInfo
+        displayInfo();
+    }
+    
+    // Create displayInfo that prints out statements to output
+    public static void displayInfo(){
+        System.out.println("Paradise Day Spa wants to pamper you.");
+        System.out.println("We will make you look good.");
+    }
+}
