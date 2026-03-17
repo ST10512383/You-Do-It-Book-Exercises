f25bea12c05714b3eb9ab5ec41cbdc0bc560f75d
Cailen Boshoff <ST10512383@myemeris.edu.za>
2026-03-17 11:37:11 +0200

    Create ParadiseInfo.java

---
+++ Java-Programming/Chapter-3/You-Do-it/ParadiseInfo/src/main/java/com/mycompany/paradiseinfo/ParadiseInfo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.paradiseinfo;
+
+/**
+ *
+ * @author CALBIX
+ */
+public class ParadiseInfo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
