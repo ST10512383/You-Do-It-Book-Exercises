8d8c7b5050775783e247c4c181d665e6b1dab001
Cailen Boshoff <ST10512383@myemeris.edu.za>
2026-03-18 12:09:49 +0200

    Update ParadiseInfo2.java

--- Java-Programming/Chapter-3/You-Do-it/ParadiseInfo/src/main/java/com/mycompany/paradiseinfo/ParadiseInfo2.java
+++ Java-Programming/Chapter-3/You-Do-it/ParadiseInfo/src/main/java/com/mycompany/paradiseinfo/ParadiseInfo2.java
@@ -1,10 +1,38 @@
 package com.mycompany.paradiseinfo;
 
+// Import JOptionPane
+import javax.swing.JOptionPane;
+
 public class ParadiseInfo2 {
 
     public static void main(String[] args) {
-        // Invoke displayInfo
-        displayInfo();
+        
+        // Declare three variables
+        double price;
+        double discount;
+        double savings;
+        
+        // Ask user for full price with GUI
+        price = Double.parseDouble(JOptionPane.showInputDialog("Enter cutoff price for discount"));
+        
+        // Ask user for discount with GUI
+        discount = Double.parseDouble(JOptionPane.showInputDialog("Enter discount rate as a whole number"));
+        
+        // Assign method to variable
+        savings = computeDiscountInfo(price, discount);
+        
+        // Concatinate strings into one variable
+        String message = "Special this week on any service over " + price + " Discount of " + discount + " percent " + "That's savings of at least $" + savings;
+        
+        // Use JOptionPane to show GUI messages to the customer
+        JOptionPane.showMessageDialog(null, message);
+    }
+    
+    // Calculate Discount and return the discount
+    public static double computeDiscountInfo(double price, double discountRate){
+        double savings;
+        savings = price * discountRate / 100;
+        return savings;
     }
     
     // Create displayInfo that prints out statements to output
