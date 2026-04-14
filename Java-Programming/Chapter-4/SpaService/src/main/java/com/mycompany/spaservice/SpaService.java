4cc0e22b9f7d78cffb69fc2b9ac876280b930457
Cailen Boshoff <ST10512383@myemeris.edu.za>
2026-04-14 14:46:20 +0200

    Create SpaService Class

--- Java-Programming/Chapter-4/SpaService/src/main/java/com/mycompany/spaservice/SpaService.java
+++ Java-Programming/Chapter-4/SpaService/src/main/java/com/mycompany/spaservice/SpaService.java
@@ -1,16 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.mycompany.spaservice;
 
-/**
- *
- * @author CALBIX
- */
 public class SpaService {
-
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
+    
 }
