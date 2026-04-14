98498b0d5904c002a027c489ca0712dc448714ad
Cailen Boshoff <ST10512383@myemeris.edu.za>
2026-04-14 15:55:29 +0200

    Fix logic error

--- Java-Programming/Chapter-4/SpaService/src/main/java/com/mycompany/spaservice/CreateSpaServices.java
+++ Java-Programming/Chapter-4/SpaService/src/main/java/com/mycompany/spaservice/CreateSpaServices.java
@@ -58,7 +58,7 @@ public class CreateSpaServices {
         System.out.println(firstService.getServiceDescription() + " $" + firstService.getPrice());
         
         // Display secondService details
-        System.out.println("First service details: ");
-        System.out.println(firstService.getServiceDescription() + " $" + firstService.getPrice());
+        System.out.println("Second service details: ");
+        System.out.println(secondService.getServiceDescription() + " $" + secondService.getPrice());
     }
 }
