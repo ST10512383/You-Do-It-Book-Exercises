92b999dc92b36d8fe371a5f5f09aa9208b7e9857
Cailen Boshoff <ST10512383@myemeris.edu.za>
2026-04-14 14:51:44 +0200

    Add getter and setter methods

--- Java-Programming/Chapter-4/SpaService/src/main/java/com/mycompany/spaservice/SpaService.java
+++ Java-Programming/Chapter-4/SpaService/src/main/java/com/mycompany/spaservice/SpaService.java
@@ -1,5 +1,22 @@
 package com.mycompany.spaservice;
 
 public class SpaService {
+    private String serviceDescription;
+    private double price;
     
+    public void setServiceDescription (String service) {
+        serviceDescription = service;
+    }
+    
+    public void setPrice (double servicePrice) {
+        price = servicePrice;
+    }
+    
+    public String getServiceDescription() {
+        return serviceDescription;
+    }
+    
+    public double getPrice() {
+        return price;
+    }
 }
