82d974d6f1e0527895da5a0962ea9dc6e8162cc1
Cailen Boshoff <ST10512383@myemeris.edu.za>
2026-04-14 14:54:31 +0200

    Add comments

--- Java-Programming/Chapter-4/SpaService/src/main/java/com/mycompany/spaservice/SpaService.java
+++ Java-Programming/Chapter-4/SpaService/src/main/java/com/mycompany/spaservice/SpaService.java
@@ -1,21 +1,27 @@
 package com.mycompany.spaservice;
 
 public class SpaService {
+    
+    // Declare variables for spa to use
     private String serviceDescription;
     private double price;
     
+    // Declare setter (mutator) for setServiceDescription
     public void setServiceDescription (String service) {
         serviceDescription = service;
     }
     
+    // Declare setter (mutator) for setPrice
     public void setPrice (double servicePrice) {
         price = servicePrice;
     }
     
+    // Declare getter (accessor) for getServiceDescription
     public String getServiceDescription() {
         return serviceDescription;
     }
     
+    // Declare getter (accessor) for getPrice
     public double getPrice() {
         return price;
     }
